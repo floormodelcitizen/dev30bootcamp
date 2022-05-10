9689e0cbc9519dd7cff05cb5468585750fda2947
Chad Christopher <devluxv20@gmail.com>
2022-05-09 22:13:45 -0700

    Finish Battery

--- shipwrecked/days/day15/power.js
+++ shipwrecked/days/day15/power.js
@@ -1,35 +1,67 @@
-const iron = 'Fe'
-const copper = 'Cu'
-const potassiumHydroxide = 'KOH'
-const chromium = 'Cr'
-const pyrolusite = 'MnO_2'
+// Elements and Materials
 const carbon = 'C'
 const cellulos = '(C6H10O5)n'
+const chromium = 'Cr'
+const copper = 'Cu'
+const iron = 'Fe'
+const lead = 'Pb'
+const potassiumHydroxide = 'KOH'
+const pyrolusite = 'MnO_2'
 const water = 'H2O'
 const zinc = 'Zn'
+let brass = [copper, zinc]
+let steel = [iron, chromium, carbon]
 
-
-let container;
-
-let steel = [ iron, chromium, carbon ]
-
-let cathode = [ carbon, pyrolusite ]
-
-let separator;
-
-let anode = [ zinc ]
-
+// Battery Components
+let container
+let cathode = [carbon, pyrolusite]
+let separator
+let anode = [zinc]
 let electrolyte = [potassiumHydroxide]
+let collector = [copper, zinc]
 
-let collector = [ copper, zinc ]
-
+// Battery Blueprint
 const battery = [
-  container,
-  cathode,
-  separator,
-  anode,
-  electrolyte,
-  collector = [ copper, zinc ]
+  container = [
+    steel =
+      [
+        iron,
+        chromium,
+        carbon
+      ]
+    ],
+  cathode =
+    [
+      carbon,
+      pyrolusite
+    ],
+  separator =
+    [cellulos],
+  anode =
+    [zinc],
+  electrolyte =
+    [
+      water,
+      potassiumHydroxide
+    ],
+  collector =
+    brass =
+    [
+      copper,
+      zinc,
+      lead
+    ]
 ]
 
-console.log(battery[5][0])
+console.log(`Carbon: ${battery[0][0][2]}`);
+console.log(`Cellulos: ${battery[2][0]}`);
+console.log(`Chromium: ${battery[0][0][1]}`);
+console.log(`Copper: ${battery[5][0]}`);
+console.log(`Iron: ${battery[0][0][0]}`);
+console.log(`Lead: ${battery[5][2]}`);
+console.log(`Potassium Hydroxide: ${battery[4][1]}`);
+console.log(`Pyrolusite: ${battery[1][0]}`);
+console.log(`Water: ${battery[4][0]}`);
+console.log(`Zinc I: ${battery[3][0]}`);
+console.log(`Zinc II: ${battery[5][1]}`);
+
